b4f417c765ddb3574efb24d13971444e073003bb
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-08-31 21:19:59 +0300

    Добавление и удаление элементов из двусторонней очереди

--- src/Queues/Main.java
+++ src/Queues/Main.java
@@ -1,23 +1,29 @@
 package Queues;
 
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 import static Queues.Practice.*;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> queueInt = new LinkedList<>();
+        Queue<String> queueStr = new LinkedList<>();
+        Queue<String> priorityQueueStr = new PriorityQueue<>();
+        Deque<String> stack = new LinkedList<>();
 
-        Task1(queue);
+//        Task1(queueInt);
+        Task5(stack);
 
         /*
             Task1: Создание очереди и добавление элементов
-            Task2:
-            Task3:
-            Task4:
-            Task5:
-            Task6:
+            Task2: Базовые операции с очередью: извлечение и просмотр
+            Task3: Очередь с приоритетом (PriorityQueue)
+            Task4: Использование Deque как двусторонней очереди
+            Task5: Добавление и удаление элементов из двусторонней очереди
+            Task6: Проверка наличия элемента в очереди
             Task7:
             Task8:
             Task9:
--- src/Queues/Practice.java
+++ src/Queues/Practice.java
@@ -41,4 +41,98 @@ public class Practice {
             System.out.println(queue.poll());
         }
     }
+
+    public static void Task2(Queue<String> queue) {
+        // Создание очереди и добавление элементов
+        /*
+        В Java интерфейс Queue обычно реализуется через LinkedList или PriorityQueue.
+        Основные операции включают add()/offer() для добавления элементов,
+        remove()/poll() для извлечения и peek() для просмотра первого элемента без удаления.
+         */
+
+        System.out.println("Task2: Создание очереди и добавление элементов");
+
+        queue.offer("Cherry");
+        queue.offer("Apple");
+        queue.offer("Banana");
+
+        // Вывод очереди на экран
+        System.out.println("Очередь: " + queue);
+
+        // Вывод
+        System.out.println("Первый элемент: " + queue.peek()); // Возвращает Cherry
+        System.out.println("Извлеченный  элемент: " + queue.poll()); // Возвращает и удаляет Cherry
+        System.out.println("Очередь: " + queue);
+        System.out.println("Первый элемент после извлечения: " + queue.peek()); // Возвращает Apple
+        System.out.println("Очередь: " + queue);
+    }
+
+    public static void Task3(Queue<String> priorityQueue) {
+        // PriorityQueue обеспечивает порядок элементов на основе естественного порядка или заданного компаратора.
+        // В случае строк это будет лексикографический порядок
+        System.out.println("Task3: Очередь с приоритетом (PriorityQueue)");
+
+        priorityQueue.offer("Milk");
+        priorityQueue.offer("Juice");
+        priorityQueue.offer("Tea");
+        priorityQueue.offer("Coffee");
+        priorityQueue.offer("Water");
+
+        System.out.println("Очередь с приоритетом:");
+
+        while (!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.poll());
+        }
+    }
+
+    public static void Task4(Deque<String> stack) {
+        stack.addFirst("First");
+        stack.add("Second");
+        stack.addFirst("Third");
+        stack.addLast("Last");
+
+        // Вывод двусторонней очереди
+        System.out.println("Очередь: " + stack);
+
+        System.out.println("Удалить первый: " + stack.removeFirst());
+        System.out.println("Удалить последний: " + stack.removeLast());
+    }
+
+    public static void Task5(Deque<String> stack) {
+        // Добавление и удаление элементов из двусторонней очереди
+        stack.addFirst("1");
+        stack.addFirst("2");
+        stack.addFirst("3");
+        stack.addFirst("4");
+
+        stack.addLast("5");
+        stack.addLast("6");
+        stack.addLast("7");
+
+        System.out.println("Удаление элементов из двусторонней очереди с начала:");
+
+        // Удаление элементов из двусторонней очереди с начала
+        // ВНИМАНИЕ! элементы с конца вывелись в другом порядке
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pollFirst());
+        }
+        System.out.println("Очередь после удаления с начала: " + stack);
+
+        stack.addLast("7");
+        stack.addLast("6");
+        stack.addLast("5");
+        stack.addLast("4");
+
+        stack.addFirst("3");
+        stack.addFirst("2");
+        stack.addFirst("1");
+
+        System.out.println("Удаление элементов из двусторонней очереди с конца:");
+        // Удаление элементов из двусторонней очереди с конца
+        // ВНИМАНИЕ! элементы с конца вывелись в другом порядке
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pollLast());
+        }
+        System.out.println("Очередь после удаления с конца: " + stack);
+    }
 }
