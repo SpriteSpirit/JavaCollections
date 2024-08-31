77a20cfc56a16928f7ec2c07ab3453399e33152b
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-08-31 23:31:35 +0300

    Set и Map базовая практика

---
+++ src/SetsAndMaps/Main.java
@@ -0,0 +1,20 @@
+package SetsAndMaps;
+
+
+import static SetsAndMaps.Practice.Exercise;
+
+public class Main {
+    public static void main(String[] args) {
+        Exercise();
+
+        /*
+            Task1:
+            Task2:
+            Task3:
+            Task4:
+            Task5:
+            Task6:
+
+         */
+    }
+}
